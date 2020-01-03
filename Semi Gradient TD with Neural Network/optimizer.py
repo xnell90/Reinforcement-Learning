15313c6f7478a5d3ae8561522999604cfa7826b5
Alwyn Lopez <AlwynLopez@Alwyns-Air.home>
2020-01-02 21:03:11 -0500

    Added new files instead of shortcut files

--- Semi Gradient TD with Neural Network/optimizer.py
+++ Semi Gradient TD with Neural Network/optimizer.py
@@ -1,1 +1,20 @@
-/home/jovyan/work-ro/week2/optimizer.py
+"""An abstract class that specifies the optimizer API for Agent.
+"""
+
+from abc import ABCMeta, abstractmethod
+
+class BaseOptimizer:
+	__metaclass__ = ABCMeta
+
+	def __init__(self):
+		pass
+
+	@abstractmethod
+	def optimizer_init(self, optimizer_info):
+		"""Setup for the optimizer."""
+
+	@abstractmethod
+	def update_weights(self, weights, g):
+		"""
+        Given weights and update g, return updated weights
+        """
